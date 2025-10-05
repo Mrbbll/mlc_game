ead4ca7f35482413442eca8167bf524e346d8bdf
Mr_bl <1300264929@qq.com>
2025-10-05 23:46:23 +0800

    update

--- src/main/java/com/mlc/mlcgames/Gameprepare.java
+++ src/main/java/com/mlc/mlcgames/Gameprepare.java
@@ -1,5 +1,6 @@
 package com.mlc.mlcgames;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.PlayerInventory;
 
@@ -11,6 +12,7 @@ import static com.mlc.mlcgames.Mlcgames.*;
 public class Gameprepare {
     public Gameprepare(){
         //加入在队伍的玩家
+        ingamepalyer.clear();
         ingamepalyer.addAll(teammanager.getteamplayer(ateam));
         ingamepalyer.addAll(teammanager.getteamplayer(bteam));
 
@@ -25,8 +27,8 @@ public class Gameprepare {
             teammanager.cleanTeam(bteam);
 
             //传送
-
-
+            Location location = new Location(player.getWorld(),0, 0 ,2000);
+            player.teleport(location);
             //给菜单
             inv.setItem(0, itemmanger.mlcmenu);
 

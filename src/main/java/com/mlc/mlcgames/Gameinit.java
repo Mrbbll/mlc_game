46d28eb66714198b4bfad61201d489196eb8e238
Mr_bl <1300264929@qq.com>
2025-10-13 23:10:36 +0800

    update

--- src/main/java/com/mlc/mlcgames/Gameinit.java
+++ src/main/java/com/mlc/mlcgames/Gameinit.java
@@ -20,6 +20,7 @@ public class Gameinit {
     public Gameinit(){
         bank_1 = new Location(Bukkit.getWorld("world"),0,70,0);
 
+
         bank_lobby = new Location(Bukkit.getWorld("world"),0 , 70,0 );
         bank_respawn = new Location(Bukkit.getWorld("world"),0 , 70,0);
         bank_gamemode = 0;
